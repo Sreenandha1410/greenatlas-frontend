7b679ef9bc3ce4d4d63c0733ec763a43741625a3
Tumili369 <tumilimohan@gmail.com>
2026-09-26 12:11:16 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -194,7 +194,7 @@ export default function Home() {
 
         {/* Scroll Cue */}
         <motion.div
-          className="absolute bottom-8 left-1/2 -translate-x-1/2 flex flex-col items-center gap-1"
+          className="hidden md:flex absolute bottom-8 left-1/2 -translate-x-1/2 flex-col items-center gap-1"
           animate={{ y: [0, 8, 0] }}
           transition={{ repeat: Infinity, duration: 1.8 }}
         >
